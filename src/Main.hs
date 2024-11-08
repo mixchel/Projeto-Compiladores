dfc2bf04910180c827340ba03f96d5f5ce7f454e
Michel Silva <michelccs2003@gmail.com>
2024-11-08 23:25:43 +0000

    Main.hs that should be with the last commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,45 @@
 module Main where
 import Parser (parse, Prog, AbstractSyntaxTree, parseStms)
-import Lexer (alexScanTokens)
+import Lexer (alexScanTokens, Token (RBRACE, NEWLINE, ENDOFSTATEMENT, SEMICOLON, ELSE))
 import PrettyPrint
 import System.Environment (getArgs)
-import Distribution.InstalledPackageInfo (AbiDependency)
+import GHC.IO.Handle (Newline)
+
+
+wrapRbrace :: [Token] -> [Token]
+wrapRbrace (RBRACE:xs) = NEWLINE:RBRACE:wrapRbrace xs
+wrapRbrace (x:xs) = x : wrapRbrace xs
+wrapRbrace [] = []
+
+mergeEndofStm :: [Token] -> [Token]
+mergeEndofStm (NEWLINE:xs) = ENDOFSTATEMENT: mergeEndofStm xs
+mergeEndofStm (SEMICOLON:xs) = ENDOFSTATEMENT: mergeEndofStm xs
+mergeEndofStm (x:xs) = x : mergeEndofStm xs
+mergeEndofStm [] = []
+
+removeIrrelevantNewlines :: [Token] -> [Token]
+removeIrrelevantNewlines (NEWLINE:ELSE:NEWLINE:xs) = ELSE: removeIrrelevantNewlines xs
+removeIrrelevantNewlines (NEWLINE:ELSE:xs) = ELSE: removeIrrelevantNewlines xs
+removeIrrelevantNewlines (ELSE:NEWLINE:xs) = ELSE: removeIrrelevantNewlines xs
+removeIrrelevantNewlines (NEWLINE:NEWLINE:xs) = NEWLINE: removeIrrelevantNewlines xs
+removeIrrelevantNewlines (x:xs) = x :removeIrrelevantNewlines xs
+removeIrrelevantNewlines [] = []
+
+prepareTokens :: [Token] -> [Token]
+prepareTokens = mergeEndofStm . removeIrrelevantNewlines . wrapRbrace
 
 makeAst:: String -> AbstractSyntaxTree
-makeAst = parse . alexScanTokens
+makeAst = parse . prepareTokens . alexScanTokens
 
 makeStmAst :: String -> Prog
-makeStmAst = parseStms . alexScanTokens
+makeStmAst xs = parseStms $ prepareTokens (alexScanTokens xs ++ [ENDOFSTATEMENT])
 
 main:: IO()
 main = do
     args <- getArgs
     let path = head args
     file <- readFile path
-    let tokens = alexScanTokens file
+    let tokens = prepareTokens $ alexScanTokens file
     let ast = makeAst file
     print tokens
     putStrLn ""
