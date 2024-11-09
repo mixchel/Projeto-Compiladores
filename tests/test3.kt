30ba02cf8b3b01fd608cb7598b4d91b76b2a7075
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2024-11-09 17:29:19 +0000

    fix test3.kt

--- tests/test3.kt
+++ tests/test3.kt
@@ -1,13 +1,14 @@
-
-var x = 5
-var y = 5
-if (x<6) {
-    x = 7
-    func('c')
+fun main(){
+    var x = 5
+    var y = 5
+    if (x<6) {
+        x = 7
+        func('c')
+    }
+    while (x < 3) {
+        func2(true)
+    }
+    z = 1 + 2
+    return 2
+    a = z + 5
 }
-while (x < 3) {
-    func2(true)
-}
-z = 1 + 2
-return 2
-a = z + 5
