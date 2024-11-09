7277760a7dff81880141de3cc616e74dd4efcbaa
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2024-11-09 16:47:58 +0000

    add more tests

---
+++ tests/testfib.kt
@@ -0,0 +1,16 @@
+fun main(){
+    x = 0;
+    y = 1;
+    result = 0;
+    n = 20
+    if(n < 0){
+         print("Invalid number")
+    }
+    while(true){
+         result = x + y
+         x = y
+         y = result
+         n = n - 1
+    }
+    print(result)
+}
---
+++ tests/testflowcontrol.kt
@@ -0,0 +1,29 @@
+fun main(){
+    if(true){
+        print("1st condition")
+    }
+    if(false){
+        print("wrong condition")
+    } else {
+      print ("2nd condition")
+    }
+
+    if(true)
+    {
+        print("3rd condition")
+    }
+
+    if(true){print("4th condition")}
+
+    if(false){} else {print("5th condition")}
+
+    x = true
+    while(x){x = false}
+    while(x){
+        x = false
+    }
+    while(x)
+    {
+        x = false
+    }
+}
