2c41833fe2429ae07e991039f2765f4c667a3007
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2024-12-06 19:16:27 +0000

    import types from Parser

--- src/CodeGenerator.hs
+++ src/CodeGenerator.hs
@@ -1,6 +1,6 @@
 module CodeGenerator where
 import Data.Text
--- ISSUE: importing stuff from the parser generaters a parser.hs error
+import Parser (Exp (..), Stm (..))
 
 data Instr = MOVE Temp Temp
            | MOVEI Temp Int
@@ -13,35 +13,6 @@ data Instr = MOVE Temp Temp
            | PRINT' Temp
            | RETURN' Temp
            | NEG Temp
-
-data Stm = If Exp Stm
-            | IfElse Exp Stm Stm
-            | Var Id Exp
-            | Return Exp
-            | Print Exp
-            | Block Prog
-            | While Exp Stm
-            | Assign Id Exp
-            | ExpStm Exp
-            | EmptyStm
-
--- data Exp = Plus Exp Exp | Minus Exp Exp | Times Exp Exp | Div Exp Exp | Mod Exp Exp
---         | Or Exp Exp | And Exp Exp | Not Exp
---         | Equal Exp Exp | Nequal Exp Exp | Greatereq Exp Exp | Lesseq Exp Exp | Greater Exp Exp | Less Exp Exp
---         | Int Int
---         | Readln
---         | SubExp Exp
---         | Negate Exp
---         | Identifier Id
-
--- ISSUE: Had to change Int Integer to Int Int so transExp (Int n) didn't raise a type error due to expecting an Int but getting an Integer
--- ISSUE: Had to transform the table this way to remove duplication
-data Exp = Op BinOP Exp Exp
-        | Int Int
-        | Readln
-        | SubExp Exp
-        | Negate Exp
-        | Identifier Id
 
 -- ISSUE: BinOP names conflict with data Exp operators
 data BinOP = Sum | Sub | Mult | Divide | Modulus | Lt | Lteq | Eq | Neq | Gt | Gteq
@@ -66,12 +37,12 @@ newLabel (temps, labels) = ("L"++show labels, (temps, labels+1))
 transExp :: Exp -> Table -> Temp -> Supply -> ([Instr], Supply)
 transExp (Int n) table dest supply = ([MOVEI dest n], supply)
 transExp Readln table dest supply = ([READLN], supply)
-transExp (Op op e1 e2) table dest supply
+transExp (Plus e1 e2) table dest supply -- TODO: check if there's a better way than to copy paste this stuff for every arithmetic expressions
   = let (t1, supply1) = newTemp supply
         (t2, supply2) = newTemp supply1
         (code1, supply3) = transExp e1 table t1 supply2
         (code2, supply4) = transExp e2 table t2 supply3
-        code = code1 ++ code2 ++ [OP op dest t1 t2]
+        code = code1 ++ code2 ++ [ADD dest t1 t2]
     in (code, supply4)
 transExp (Negate e) table dest supply
   = let (t1, supply1) = newTemp supply
@@ -82,13 +53,14 @@ transExp (Negate e) table dest supply
 -- ISSUE: transCond needs to know the Relational Operator to supply it to the generated intermediary code (relop)
 -- ISSUE: transCond will accept any operator (relational or arithmetic)
 transCond :: Exp -> Table -> Label -> Label -> Supply -> ([Instr], Supply)
-transCond e table l1 l2 supply = case e of
-                     Op op e1 e2 -> let (t1, supply1) = newTemp supply
-                                        (t2, supply2) = newTemp supply1
-                                        (code1, supply3) = transExp e1 table t1 supply2
-                                        (code2, supply4) = transExp e2 table t2 supply3
-                                        code = code1 ++ code2 ++ [COND t1 op t2 l1 l2]
-                                    in (code, supply4)
+transCond e table l1 l2 supply = ([], supply) --TODO: remove this line, I only added it so it would compile
+-- transCond e table l1 l2 supply = case e of
+--                      Op op e1 e2 -> let (t1, supply1) = newTemp supply
+--                                         (t2, supply2) = newTemp supply1
+--                                         (code1, supply3) = transExp e1 table t1 supply2
+--                                         (code2, supply4) = transExp e2 table t2 supply3
+--                                         code = code1 ++ code2 ++ [COND t1 op t2 l1 l2]
+--                                     in (code, supply4)
 
 -- TODO:  Var Id Exp, Block Prog, Assign Id Exp, ExpStm Exp
 -- NOTE: Var Id Exp and Assign Id Exp might require a new new function, since they must return a new table (I believe)
@@ -132,7 +104,7 @@ Statements [0/9 rules]
 Expressions [0/21]
 Prog
 Release temporary/registers (function)
-Implement table for variables (plus scoping, plus relevant information)
+Implement table for variables (plus scoping, plus relevant information) (necessary?)
 
 Generate assembly
 -}
