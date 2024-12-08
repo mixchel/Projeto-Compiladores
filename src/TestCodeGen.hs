2a431ffab45c6538c8b84e1ffbf32cbe3e3364af
Michel Castro <michelccs2003@gmail.com>
2024-12-08 17:00:39 +0000

    Better Testing for Code Generator

--- src/TestCodeGen.hs
+++ src/TestCodeGen.hs
@@ -1,10 +1,26 @@
 import AstGenerator (makeStmAst)
-import CodeGenerator 
+import CodeGenerator ( Supply, Instr, transStm )
+import Distribution.Pretty (Pretty(pretty))
+import Language.Haskell.TH.Syntax (sequenceQ)
 
-exps = ["print(1+2)","print(1+3)", "print(1*5+4-3)"]
+exps = ["print(1+2)","print(1+3)", "print(1*5+4-3)","print(1+5*8)"]
 
 test :: String -> ([Instr], Supply)
-test input = transStm (makeStmAst input) [] (0,0) 
+test input = transStm (makeStmAst input) [] (0,0)
 
 testAll :: [([Instr], Supply)]
-testAll = [test a |a <- exps]
+testAll = map test exps
+
+prettyTest :: [Char] -> [IO ()]
+prettyTest input = map print instr
+    where (instr,_) = test input
+
+prettyTestAll :: [[IO ()]]
+prettyTestAll = map prettyTest exps
+
+seqNl :: [IO ()] -> IO ()
+seqNl a = sequence_ (a ++ [putStrLn ""]) 
+
+
+main :: IO()
+main = mapM_ seqNl prettyTestAll
