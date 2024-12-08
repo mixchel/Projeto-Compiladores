5a1fbc16a56bd8f89235026efcd3322314f84737
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2024-12-08 20:51:55 +0000

    change types

---
+++ src/MipsGenerator.hs
@@ -0,0 +1,22 @@
+module MipsGenerator where
+import Data.Text
+import CodeGenerator (Instr (..))
+
+-- TODO: define asembly datatype
+--data Assembly = 
+
+type AssemblyInstr = [String]
+
+transInstr :: Instr -> AssemblyInstr
+transInstr (MOVE t1 t2) = ["addi t1, t2, $zero"]
+transInstr (MOVEI t i) = ["addi t, i, $zero"]
+--transInstr (OP op t1 t2 t3)
+--transInstr (OPI op t1 t2 i)
+transInstr (LABEL l) = ["l ++ : "]
+transInstr (JUMP l) = ["j l"]
+--transInstr (COND Temp BinOP Temp Label Label)
+transInstr (READLN) = ["la $a0, target", "li $v0, 5", "syscall"]
+transInstr (PRINT' t) = ["la $a0, target", "li $v0, 1", "syscall"]
+transInstr (RETURN' t) = ["add $v0, t, $zero", "jr $ra"]
+transInstr (NEG t) = ["subu t, $zero, t"]
+--transInstr (NOT t) = ["sltu t, $zero, t", "xori t, t, 1"]
