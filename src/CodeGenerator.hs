0b5947ad01103984688c96a20c118db2c237fa08
Michel Castro <michelccs2003@gmail.com>
2024-12-09 01:12:53 +0000

    new Way to Store State (WIP) only implemented for statement and Cond

--- src/CodeGenerator.hs
+++ src/CodeGenerator.hs
@@ -1,6 +1,7 @@
 module CodeGenerator where
 import Data.Text
 import Parser (Exp (..), Stm (..))
+import qualified Data.Map as Map
 
 data Instr = MOVE Temp Temp
            | MOVEI Temp Int
@@ -17,6 +18,10 @@ data Instr = MOVE Temp Temp
     deriving Show
 
 -- ISSUE: Ambiguity between Parsers Exp And/Or labels and Cond BinOP And/Or
+data State = State {table:: Map.Map String Int, register :: Int, label :: Int}
+    deriving Show
+
+initialState = State {table = Map.Empty, registerCount = 0, labelCount = 0}
 data BinOP = Sum | Sub | Mult | Divide | Modulus | Lt | Lteq | Eq | Neq | Gt | Gteq | AndC | OrC
     deriving Show
 type Prog = [Stm]
@@ -26,17 +31,33 @@ type Label = String
 type Table = [(String, Int)]
 type Supply = (Int, Int)
 
+
 newTemp :: Supply -> (Temp, Supply)
 newTemp (temps, labels) = ("t"++show temps, (temps +1, labels))
+
+newTemp' :: State -> (Temp, State)
+newTemp' s = ('t':show temp, s {registerCount = temp +1})
+    where temp = registerCount s
 
 newLabel :: Supply -> (Label, Supply)
 newLabel (temps, labels) = ("l"++show labels, (temps, labels+1))
 
+newLabel' :: State -> (Temp, State)
+newLabel' s = ('l':show label, s {labelCount = label+1})
+    where label = register s
+
 popTemp :: Int -> Supply -> Supply
 popTemp x (temps, labels) = (temps - x, labels)
 
--- TODO: subexp, identifier
+popTemp' :: Int -> State -> State
+popTemp x s = s {registerCount = currCount - x}
+    where currCount = registerCount s
+
 -- TODO: check if there's a better way than to copy paste this stuff for every arithmetic expressions
+
+transExp' :: Exp -> Temp -> State -> ([Instr], State)
+
+
 transExp :: Exp -> Table -> Temp -> Supply -> ([Instr], Supply)
 transExp (Int n) table dest supply = ([MOVEI dest n], supply)
 transExp Readln table dest supply = ([READLN], supply)
@@ -89,6 +110,62 @@ transExp (SubExp e) table dest supply = transExp e table dest supply
 
 -- TODO: Is it necessary to implement the relational OP (==, <, >, <=, >=) in transExp if I already have them here?
 -- TODO: necessary to consider that a condition may be a SUM, a SUB, a DIV, etc...?
+transCond':: Exp -> Label -> Label -> State -> ([Instr], State)
+transCond' e l1 l2 s = case e of
+    Equal e1 e2 -> let (t1, state1) = newTemp' s
+                       (t2, state2) = newTemp' state1
+                       (code1, state3) = transExp e1 state2 
+                       (code2, state4) = transExp e2 state3
+                       code = code1 ++ code2 ++ [COND Eq t1 t2 l1 l2]
+                    in (code, state4)
+    Nequal e1 e2 -> let (t1, state1) = newTemp' s
+                        (t2, state2) = newTemp' state1
+                        (code1, state3) = transExp e1 state2 
+                        (code2, state4) = transExp e2 state3
+                        code = code1 ++ code2 ++ [COND Neq t1 t2 l1 l2]
+                    in (code, state4)
+    Greatereq e1 e2 -> let (t1, state1) = newTemp' s
+                           (t2, state2) = newTemp' state1
+                           (code1, state3) = transExp e1 state2 
+                           (code2, state4) = transExp e2 state3
+                           code = code1 ++ code2 ++ [COND Gteq t1 t2 l1 l2]
+                        in (code, state4)
+    Lesseq e1 e2 -> let (t1, state1) = newTemp' s
+                        (t2, state2) = newTemp' state1
+                        (code1, state3) = transExp e1 state2 
+                        (code2, state4) = transExp e2 state3
+                        code = code1 ++ code2 ++ [COND Lteq t1 t2 l1 l2]
+                    in (code, state4)
+    Greater e1 e2 -> let (t1, state1) = newTemp' s
+                         (t2, state2) = newTemp' state1
+                         (code1, state3) = transExp e1 state2 
+                         (code2, state4) = transExp e2 state3
+                         code = code1 ++ code2 ++ [COND Gt t1 t2 l1 l2]
+                     in (code, state4)
+    Less e1 e2 -> let (t1, state1) = newTemp' s
+                      (t2, state2) = newTemp' state1
+                      (code1, state3) = transExp e1 state2 
+                      (code2, state4) = transExp e2 state3
+                      code = code1 ++ code2 ++ [COND Lt t1 t2 l1 l2]
+                  in (code, state4)
+    And e1 e2 -> let (t1, state1) = newTemp' s
+                     (t2, state2) = newTemp' state1
+                     (code1, state3) = transExp e1 state2 
+                     (code2, state4) = transExp e2 state3
+                     code = code1 ++ code2 ++ [COND Andc t1 t2 l1 l2]
+                 in (code, state4)
+    Or e1 e2 -> let (t1, state1) = newTemp' s
+                    (t2, state2) = newTemp' state1
+                    (code1, state3) = transExp e1 state2 
+                    (code2, state4) = transExp e2 state3
+                    code = code1 ++ code2 ++ [COND OrC t1 t2 l1 l2]
+                in (code, state4)
+    Not e -> let (t1, state1) = newTemp' s
+                 (t2, state2) = newTemp' state1
+                 (code1, state3) = transExp e1 state2 
+                 code = code1 ++ [COND Andc t1 t1 l1 l2]
+             in (code, state3)
+ 
 transCond :: Exp -> Table -> Label -> Label -> Supply -> ([Instr], Supply)
 transCond e table l1 l2 supply = case e of
                      Equal e1 e2 -> let (t1, supply1) = newTemp supply
@@ -148,6 +225,39 @@ transCond e table l1 l2 supply = case e of
 
 -- NOTE: Var Id Exp and Assign Id Exp might require a new new function, since they must return a new table (I believe)
 -- TODO: The RETURN instruction, is it necessary? (since we dont have function calling)
+transStm' :: Stm -> State -> ([Instr], Supply)
+tranStm' EmptyStm s = ([], s)
+transStm' stm s = case stm of
+  (If e stm) -> let (l1, state1) = newLabel' s
+                    (l2, state2) = newLabel' state1
+                    (code1, state3) = transCond' e l1 l1 state2
+                    (code2 , state4) = transStm' e state3
+                    code = code1 ++ [LABEL l1] ++ code2 ++ [LABEL l2]
+                in (code, state4)
+  (IfElse e stm1 stm2) -> let (l1, state1) = newLabel' s
+                              (l2, state2) = newLabel' state1
+                              (l3, state3) = newLabel' state2
+                              (code1, state4) = newLabel' state3
+                              (code2, state5) = newLabel' state4
+                              (code3, state6) = newLabel' state5
+                              code = code1 ++ [LABEL l1] ++ code2 ++ [JUMP l3] ++ [LABEL l2] ++ code3 ++ [LABEL l3]
+                          in (code, supply6)
+  (While e stm) -> let (l1, state1) = newLabel' s
+                       (l2, state2) = newLabel' state1
+                       (l3, state3) = newLabel' state2
+                       (code1, state4) = transCond' e l2 l3 state3
+                       (code2, state5) = transStm' stm state4
+                       code = [LABEL l1] ++ code1 ++ [LABEL l2] ++ code2 ++ [LABEL l1] ++ [LABEL l3]
+                    in (code, state5)
+  (Print e)  -> let (t1, state1) = newTemp' s
+                    (code1, state2) = transExp' state1
+                    code = code1 ++ [PRINT' t1]
+                in (code, state2)
+  (Return e) -> let (t1, state1) = newTemp' s
+                    (code1, state2) = transExp' e t1 state1
+                    code = code1 ++ [RETURN' t1]
+                in (code, state2) 
+                                                   
 transStm :: Stm -> Table -> Supply -> ([Instr], Supply)
 transStm EmptyStm table supply = ([], supply)
 transStm stm table supply = case stm of
@@ -156,7 +266,7 @@ transStm stm table supply = case stm of
                     (code1, supply3) = transCond e table l1 l2 supply2
                     (code2, supply4) = transStm stm table supply3
                     code = code1 ++ [LABEL l1] ++ code2 ++ [LABEL l2]
-                in (code, supply4)
+                in (code, supply6)
   (IfElse e stm1 stm2) -> let (l1, supply1) = newLabel supply
                               (l2, supply2) = newLabel supply1
                               (l3, supply3) = newLabel supply2
