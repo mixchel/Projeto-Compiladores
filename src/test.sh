6e16761d7ac31222f782cf2bac87d7c157bb3d0f
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2024-12-10 21:03:15 +0000

    update test script

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-./Main ../tests/test1.kt
-sleep 2
-./Main ../tests/test2.kt
-sleep 2
-./Main ../tests/test3.kt
-sleep 2
-./Main ../tests/testfib.kt
-sleep 2
-./Main ../tests/testsimple.kt
-sleep 2
-./Main ../tests/testflowcontrol.kt
-sleep 2
-./Main ../tests/simple2.kt
-sleep 2
+for filename in ../tests/*.kt; do
+    echo 'Testing: ' "$filename"
+    ./Main "$filename"
+    sleep 1
+done
