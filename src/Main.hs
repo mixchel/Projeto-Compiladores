4bb2cbcda95017bf78fc8ba4cc6a6b46fc27df7f
Michel Castro <michelccs2003@gmail.com>
2024-12-10 17:21:15 +0000

    Made Main write resuliting assembly to file

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,16 @@
 module Main where
-import TestCodeGen (printResult, testProg)
+import TestCodeGen (printResult, testProg, Result (assembly))
 import System.Environment (getArgs)
+import System.FilePath.Posix (replaceExtension)
 
 main :: IO()
 main = 
     do 
         args <- getArgs
         let path = head args
+        let outputPath = replaceExtension path ".asm"
         file <- readFile path
-        sequence_ (printResult (testProg file))
+        let result = testProg file
+        sequence_ (printResult result)
+        let asm = unlines (assembly result)
+        writeFile outputPath asm
