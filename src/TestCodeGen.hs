399da1018caefb672d6f988b2d9933dc40392de2
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2024-12-10 00:23:53 +0000

    remove redundant test cases

--- src/TestCodeGen.hs
+++ src/TestCodeGen.hs
@@ -9,13 +9,11 @@ import System.Environment (getArgs)
 -- TODO: add tests for while, CONDs with arithmetic, READLN, var assignment, scopes, types, arithmetic with immediate values and w/vars
 stms :: [String]
 stms = ["print(1+2)",
-        "print(1+3)",
         "print(1*5+4-3)",
         "print(1+5*8)",
         "print(1*(5-1))",
         "print((5-1)*4)",
         "if (5<6) return 5 else return 2",
-        --"if (5+6) return 5 else return 2",
         "if (5<2) return 5 else {print(1) print(2)}",
         "if (5<2) {print(4) return 5} else return 3",
         "if (5<2) {if (5>2) return 4 if (5<3) return 5} else return 3",
